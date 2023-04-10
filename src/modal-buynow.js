13871bd39dc66f0fb4213db1b25984e33e96a605
Kateryna <katerynababiak@gmail.com>
2023-04-10 21:58:52 +0300

    comm

--- src/modal-buynow.js
+++ src/modal-buynow.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-buynow]"),
+    closeModalBtn: document.querySelector("[data-modal-close-buynow]"),
+    modal: document.querySelector("[data-modal-buynow]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
